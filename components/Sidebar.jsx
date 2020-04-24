8a62e39cef5970479a4175f8fbd636bc6df9dd83
Aaron Smith <aaroncosmith@gmail.com>
2020-04-24 11:31:19 -0400

    added responsive user sidebar, might break some styles but that's okay! I working on the rest of the components

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,46 +1,27 @@
 import React from "react";
 import { Component } from "react";
+import SidebarUser from "./SidebarUser"
 import styled from "styled-components";
 
 class Sidebar extends Component {
-  render() {
-    return (
-      <SideNav>
-        <SideNavA href="#">Test</SideNavA>
-        <SideNavA href="#">Test</SideNavA>
-        <SideNavA href="#">Test</SideNavA>
-        <SideNavA href="#">Test</SideNavA>
-        <SideNavA href="#">Test</SideNavA>
-      </SideNav>
-    );
-  }
+    render() {
+        return (
+            <SideNav>
+                <SidebarUser/>
+                <SidebarUser/>
+                <SidebarUser/>
+            </SideNav>
+        );
+    }
 }
 
 const SideNav = styled.div`
-display: flex;
-align-items: center;
-  height: 90%;
-  width: 20%;
-position: relative;
-flex-direction: column;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  padding: 0.75rem 0 !important;
-  margin-right: 0.1vw;
-  overflow-y: auto;
-  white-space: nowrap;
-  overflow-x: hidden;
-  background: black;
-  opacity: 0.5;
-  border-radius: 15px !important;
-`;
-const SideNavA = styled.a`
-  padding: 6px 8px 6px 16px;
-  text-decoration: none;
-  font-size: 25px;
-  color: #818181;
-  display: block;
+    display: flex;
+    width: 240px;
+    flex-direction: column;
+    background: black;
+    opacity: 0.5;
+    border-radius: 15px !important;
 `;
 
 export default Sidebar;
---
+++ components/SidebarUser.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Component } from "react";
+// import user from "./user.png"
+import styled from "styled-components";
+
+class SidebarUser extends Component {
+    render() {
+        return (
+            <UserWapper>
+                    <ImageWrapper>
+                        <UserImage src="" atl="user image"/>
+                    </ImageWrapper>
+                    <InfoWrapper>
+                        <UserName>A person</UserName>
+                        <LastMessage>Blah Blah Blah Blah!</LastMessage>
+                        <Time>4:20 PM</Time>
+                    </InfoWrapper>
+            </UserWapper>
+        );
+    }
+}
+
+const UserWapper = styled.div`
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #818181;
+    display: block;
+    border: 2px solid blue;
+    border-radius: 5px;
+    @media only screen and (min-width : 50px) and (max-width : 530px){
+        display: flex;
+        flex-direction: column;
+    }
+`
+const ImageWrapper = styled.div`
+`
+
+const UserImage = styled.img`
+    float: left;
+    max-width: 60px;
+    width: 100%;
+    margin-right: 20px;
+    margin-top: 10px;
+    border-radius: 50%;
+    @media only screen and (min-width : 50px) and (max-width : 530px){
+        height: 100%;
+        margin: 0px;
+    }
+`
+const InfoWrapper = styled.div`
+`
+const UserName = styled.p`
+    margin: 3px;
+
+    @media only screen and (min-width : 50px) and (max-width : 530px){
+        font-size: 20px;
+        white-space: wrap;
+    }
+`
+const LastMessage = styled.p`
+    font-size: 12px;
+    margin-top: 4px;
+    @media only screen and (min-width : 50px) and (max-width : 530px){
+        display: none;
+    }
+`
+const Time = styled.p`
+    font-size: 10px;
+    margin: 0px;
+    margin-bottom: 5px;
+    @media only screen and (min-width : 50px) and (max-width : 530px){
+        display: none;
+    }
+`
+
+export default SidebarUser;
