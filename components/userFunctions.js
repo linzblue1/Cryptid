7b28b816ab90129fa3b4e4272446dab27ef795d4
Blue Lindsey <linzblue5@gmail.com>
2020-04-27 18:20:32 -0500

    Fixed Cors error

--- components/userFunctions.js
+++ components/userFunctions.js
@@ -5,38 +5,38 @@ import axios from "axios";
 const options = {
     credentials: "include",
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
+        Accept: "application/json",
+        "Content-Type": "application/json"
     },
-    
+
 };
 
 export const signup = (newUser) => {
-    return axios 
-    .post("localhost:3001/users/signup", {
-        name: newUser.name,
-        email: newUser.email,
-        password: newUser.password
-    },options)
-   
-    .then(response => {
-        console.log("User is now registered")
-    })
+    return axios
+        .post("http://localhost:3001/users/signup", {
+            name: newUser.name,
+            email: newUser.email,
+            password: newUser.password
+        }, options)
+
+        .then(response => {
+            console.log("User is now registered")
+        })
 
 }
 
 export const login = (user) => {
-    return axios 
-    .post("users/login", {
-        username: user.username,
-        password:username.password
-    })
-    .then(response => {
-        localStorage.setItem('usertoken', response.data)
-        return response.data
-    })
-    .catch(err => {
-        console.log(err)
-    })
+    return axios
+        .post("users/login", {
+            username: user.username,
+            password: username.password
+        })
+        .then(response => {
+            localStorage.setItem('usertoken', response.data)
+            return response.data
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
 
